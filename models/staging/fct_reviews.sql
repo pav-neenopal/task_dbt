0e22e512274911ffa3e8356528b18049c0805ace
Pavan kumar <pavan.kumar@neenopal.com>
2026-04-09 05:17:29 +0000

    Fixed fct_reveiws

--- models/staging/fct_reviews.sql
+++ models/staging/fct_reviews.sql
@@ -1,6 +1,6 @@
 {{ config(
   materialized='incremental',
-  schema = env_var('DBT_SCHEMA_SECRET','fallback_schema'),
+  schema = env_var('DBT_SCHEMA_SECRET'),
   incremental_strategy='insert_overwrite',
   partition_by={'field': 'review_date', 'data_type': 'date'},
   incremental_predicates=["review_date >= date_trunc('month', CURRENT_DATE)"]
